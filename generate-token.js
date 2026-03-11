376407db3d9747250cd3839e892774a7ad1d959e
marcelooliveiracardoso <dev.nr.brazil@gmail.com>
2026-03-11 13:43:17 -0300

    fix: correct comment for user role in token generation

--- generate-token.js
+++ generate-token.js
@@ -18,7 +18,7 @@ const payload = {
   email: "paciente@teste.com",
   user_metadata: {
     tenant_id: "c56a4180-65aa-42ec-a945-5fd21dec0538", // ID da Clínica
-    role: "doctor" // Role do seu schema access
+    role: "doctor" // Role do seu schema access : doctot ou patient
   }
 };
 
