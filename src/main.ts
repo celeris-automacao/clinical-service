02a3c6104b405b4d5fb9a33a7ce2200b2ba41489
marcelooliveiracardoso <marcelo.oliveiracadoso@gmail.com>
2026-04-15 00:49:27 -0300

    feat: initialize clinical service with NestJS, global validation pipes, and Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  app.enableCors(); // Habilita chamadas de outras portas (CORS)
   app.setGlobalPrefix('v1');
   
   app.useGlobalPipes(new ValidationPipe({ 
